ea5a3ef3c2deb6160f7d901d39fb2aa47ecb4fd7
Rayden Kwuan Hoong Leong <rleong@c2r5s13.42singapore.sg>
2024-05-18 20:34:57 +0800

    initial commit.

---
+++ libft.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rleong <rleong@student.42singapore.sg>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/18 18:40:29 by rleong            #+#    #+#             */
+/*   Updated: 2024/05/18 20:01:32 by rleong           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+int	ft_isalnum(int c);
+int	ft_isalpha(int c);
+int	ft_isascii(int c);
+int	ft_isdigit(int c);
+int	ft_islower(int c);
+int	ft_isprint(int c);
+int	ft_isupper(int c);
+int	ft_strlen(int c);
+int	ft_tolower(int c);
+int	ft_toupper(int c);
+#endif
